94750020fb1e976819b50082989602f6abb5f358
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-08-09 09:54:08 +0000

    add get_all_members endpoint

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,13 +13,38 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": self.last_name,
+            "age":33,
+            "lucky_numbers":[7,13,22]
+        },
+        {
+
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": self.last_name,
+            "age":35,
+            "lucky_numbers":[10,14,3]
+        },
+
+        {
+
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": self.last_name,
+            "age":5,
+            "lucky_numbers":[1]
+        }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
-    def add_member(self, member):
+    def add_member(self, member, id, first_name,):
         # fill this method and update the return
         pass
 
@@ -32,5 +57,5 @@ class FamilyStructure:
         pass
 
     # this method is done, it returns a list with all the family members
-    def get_all_members(self):
-        return self._members
+    def get_all_members(self): 
+        return list(map(lambda item: [item["first_name"],item["id"]], self._members))
